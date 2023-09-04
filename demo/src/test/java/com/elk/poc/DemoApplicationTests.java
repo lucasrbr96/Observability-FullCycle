3295712dd39265cc880e51e0554ed63e0ec438fc
Lucas Ramos <lucasramosgab90@gmail.com>
2023-09-04 16:39:00 -0300

    first commit

---
+++ demo/src/test/java/com/elk/poc/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elk.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
